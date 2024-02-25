9a992c1ef3fe18eea160a5b7608da642396240a6
MhdMuzammilN <muzamil06n@gmail.com>
2024-02-26 02:13:28 +0530

    appbar conflit

--- lib/view/default_screen/default_screen.dart
+++ lib/view/default_screen/default_screen.dart
@@ -12,10 +12,10 @@ class DefaultScreen extends StatefulWidget {
 class _DefaultScreenState extends State<DefaultScreen> {
   int navigationIndex = 0;
   List bottomNavigationList = [
-    const MessagesScreen(),
-    const NotificationScreen(),
-    const MessagesScreen(),
-    const MessagesScreen(),
+    {"screen": const MessagesScreen(), "screenName": "Messages"},
+    {"screen": const NotificationScreen(), "screenName": "Notifications"},
+    {"screen": const MessagesScreen(), "screenName": "Messages"},
+    {"screen": const MessagesScreen(), "screenName": "Messages"},
   ];
 
   @override
@@ -23,90 +23,95 @@ class _DefaultScreenState extends State<DefaultScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.arrow_back_ios_new),
+        title: Text("Notification"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
         forceMaterialTransparency: true,
+        titleTextStyle: const TextStyle(
+            fontSize: 26,
+            color: Color(0xff198EB6),
+            fontWeight: FontWeight.bold),
       ),
-      bottomNavigationBar: SizedBox(
-        // height: 83,
-        child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            unselectedItemColor: Colors.black,
-            selectedItemColor: Colors.black,
-            iconSize: 24,
-            currentIndex: navigationIndex,
-            onTap: (value) {
-              navigationIndex = value;
-              setState(() {});
-            },
-            items: [
-              BottomNavigationBarItem(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.black,
+          iconSize: 24,
+          currentIndex: navigationIndex,
+          onTap: (value) {
+            navigationIndex = value;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: navigationIndex == 0
+                      ? const Color(0xff198EB6)
+                      : Colors.transparent,
+                ),
+                child: const Icon(
+                  Icons.home,
+                ),
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
                 icon: Container(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: navigationIndex == 0
+                    color: navigationIndex == 1
                         ? const Color(0xff198EB6)
                         : Colors.transparent,
                   ),
                   child: const Icon(
-                    Icons.home,
+                    Icons.schedule,
                   ),
                 ),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                  icon: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12, vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: navigationIndex == 1
-                          ? const Color(0xff198EB6)
-                          : Colors.transparent,
-                    ),
-                    child: const Icon(
-                      Icons.schedule,
-                    ),
+                label: "Notification"),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 13, vertical: 13),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: navigationIndex == 2
+                        ? const Color(0xff198EB6)
+                        : Colors.transparent,
                   ),
-                  label: "Notification"),
-              BottomNavigationBarItem(
-                  icon: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 13, vertical: 13),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: navigationIndex == 2
-                          ? const Color(0xff198EB6)
-                          : Colors.transparent,
-                    ),
-                    child: const Icon(
-                      Icons.chat,
-                    ),
+                  child: const Icon(
+                    Icons.chat,
                   ),
-                  label: "Messages"),
-              BottomNavigationBarItem(
-                  icon: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12, vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: navigationIndex == 3
-                          ? const Color(0xff198EB6)
-                          : Colors.transparent,
-                    ),
-                    child: const Icon(
-                      Icons.account_circle,
-                    ),
+                ),
+                label: "Messages"),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: navigationIndex == 3
+                        ? const Color(0xff198EB6)
+                        : Colors.transparent,
                   ),
-                  label: "Profile")
-            ]),
-      ),
+                  child: const Icon(
+                    Icons.account_circle,
+                  ),
+                ),
+                label: "Profile")
+          ]),
       // body: const MessagesScreen(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: bottomNavigationList.elementAt(navigationIndex),
+        child: bottomNavigationList[navigationIndex],
       ),
     );
   }
